d9e7194be4cab67a9bc6374ae6f5625e599aa232
johnnycheukijs <johnnycheuk@instajs.com>
2024-05-23 18:54:28 +0800

    Update build version

--- test/data/config.js
+++ test/data/config.js
@@ -1,15 +1,15 @@
-// module.exports = {
-//     SCRAPER_API_KEY: 'scp-live-2e504127f4ea404bb01276f852ec30ba',
-//     TWITTER_USERNAME: 'CheukJohnn835',
-//     TWITTER_PASSWORD: 'Since1994',
-//     TWITTER_EMAIL_ADDRESS: 'johnnycheuk9686@gmail.com'
-
-// }
-
 module.exports = {
     SCRAPER_API_KEY: 'scp-live-2e504127f4ea404bb01276f852ec30ba',
-    TWITTER_USERNAME: 'john791280',
-    TWITTER_PASSWORD: 'John12345',
-    TWITTER_EMAIL_ADDRESS: 'txnclone002@gmail.com'
+    TWITTER_USERNAME: 'CheukJohnn835',
+    TWITTER_PASSWORD: 'Since1994',
+    TWITTER_EMAIL_ADDRESS: 'johnnycheuk9686@gmail.com'
 
 }
+
+// module.exports = {
+//     SCRAPER_API_KEY: 'scp-live-2e504127f4ea404bb01276f852ec30ba',
+//     TWITTER_USERNAME: 'john791280',
+//     TWITTER_PASSWORD: 'John12345',
+//     TWITTER_EMAIL_ADDRESS: 'txnclone002@gmail.com'
+
+// }
