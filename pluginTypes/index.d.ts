3404d2bec5d3ac998371e19e72e839ab1d7fe832
johnnycheuk <johnnycheuk2649@gmail.com>
2024-09-03 15:15:16 +0800

    Rebuild with ijstech compiler

---
+++ pluginTypes/index.d.ts
@@ -0,0 +1,98 @@
+/// <amd-module name="@scom/scom-twitter-scraper-ui/index.css.ts" />
+declare module "@scom/scom-twitter-scraper-ui/index.css.ts" {
+    export const tweetPreviewStyle: string;
+    export const paginationStyle: string;
+    export const textCenterStyle: string;
+}
+/// <amd-module name="@scom/scom-twitter-scraper-ui" />
+declare module "@scom/scom-twitter-scraper-ui" {
+    import { ControlElement, Module } from '@ijstech/components';
+    interface ITweet {
+        conversationId: string;
+        id: string;
+        hashtags: any[];
+        likes: number;
+        mentions: any[];
+        name: string;
+        permanentUrl: string;
+        photos: any[];
+        replies: number;
+        retweets: number;
+        text: string;
+        thread: any[];
+        urls: [];
+        userId: string;
+        username: string;
+        videos: any[];
+        isQuoted: boolean;
+        isReply: boolean;
+        isRetweet: boolean;
+        isPin: boolean;
+        sensitiveContent: boolean;
+        timeParsed: Date;
+        timestamp: number;
+        html: string;
+        views: number;
+    }
+    global {
+        namespace JSX {
+            interface IntrinsicElements {
+                ['scom-social--import-tweets-module']: ImportTweetsModuleElement;
+            }
+        }
+    }
+    interface ImportTweetsModuleElement extends ControlElement {
+        refreshPosts: () => Promise<void>;
+        onSubmit: (tweets: ITweet) => Promise<void>;
+    }
+    interface IImportTweetsOption {
+        scraperBotApiBaseUrl: string;
+    }
+    export class ImportTweetsModule extends Module {
+        private btnSearch;
+        private btnBack;
+        private btnImport;
+        private edtName;
+        private lbWarn;
+        private switchMaxTweets;
+        private edtMaxTweets;
+        private switchSince;
+        private edtSince;
+        private edtTil;
+        private pnlSearch;
+        private pnlLoading;
+        private pnlFailed;
+        private lbFailedMessage;
+        private lbDateRangeError;
+        private lbMaxTweetsError;
+        private pnlResult;
+        private tweetsList;
+        private lbTweetsCount;
+        private lbSelectedTweetsCount;
+        private chkAll;
+        private pgnTweets;
+        private allTweets;
+        private checkedTweets;
+        private isImporting;
+        private _scraperBotApiBaseUrl;
+        onSubmit: (tweets: ITweet[]) => Promise<void>;
+        refreshPosts: () => Promise<void>;
+        constructor(options: IImportTweetsOption);
+        init(): void;
+        clear(): void;
+        private getTweets;
+        private handleSearch;
+        private handlePageChange;
+        private handleBackClick;
+        private handleSubmit;
+        private resetInputs;
+        private enableInputs;
+        private onNameChanged;
+        private handleSwitchMaxTweetsChanged;
+        private handleSwitchSinceChanged;
+        private handleTweetCheckboxChanged;
+        private handleCheckAllChanged;
+        renderTweet(tweet: any): any;
+        render(): any;
+    }
+}
--- types/index.css.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const tweetPreviewStyle: string;
-export declare const paginationStyle: string;
-export declare const textCenterStyle: string;
