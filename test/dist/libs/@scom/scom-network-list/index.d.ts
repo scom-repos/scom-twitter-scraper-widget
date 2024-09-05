359089a99f1688083473236d92c28a36b274c200
YC Wong <66290913+yc-wong@users.noreply.github.com>
2024-09-05 21:03:18 +0800

    update .gitignore

--- test/dist/libs/@scom/scom-network-list/index.d.ts
+++
@@ -1,6 +0,0 @@
-/// <amd-module name="@scom/scom-network-list" />
-declare module "@scom/scom-network-list" {
-    import { INetwork } from "@ijstech/eth-wallet";
-    export default function getNetworkList(): INetwork[];
-    export { getNetworkList };
-}
